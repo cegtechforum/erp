a0054753bc6a37f848979ebcd7356352296fe5a7
Selva Ganesh S <sselvaganesh042@gmail.com>
2024-10-30 14:38:39 +0530

    dashboard frontend

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,36 +1,76 @@
 "use client"
 import { useState,useEffect } from "react";
 import  Image  from 'next/image';
+import { Button } from "antd";
 
 export default function Dashboard(){
+
+  
+        const event = [{
+            eventId: 1,
+            eventName: "Kurushetra",
+            description: "A symposium on the latest tech trends and innovations.",
+            rollNo: "2022103012",
+            contact: "48611481",
+            organizerName: "Hariguru",
+            domain: "Tech-ops",
+            status: "upcoming",
+            date: "2024-11-10",
+            startTime: "10:00",
+            endTime: "14:00",
+        },
+        {
+            eventId: 1,
+            eventName: "Kurushetra",
+            description: "A symposium on the latest tech trends and innovations.",
+            rollNo: "2022103012",
+            contact: "48611481",
+            organizerName: "Hariguru",
+            domain: "Tech-ops",
+            status: "upcoming",
+            date: "2024-11-10",
+            startTime: "10:00",
+            endTime: "14:00",
+        },];
+
+    //get the events from the server
+
     
+    const[events,setEvents] = useState([])
+
+    useEffect(() => {
+        // fetch events from server 
+        setEvents(event)
+    }, [])
+
     return(
-        <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center text-center" >
-            <h1 className="p-5 w-[100vw] text-center text-3xl " >Dashboard</h1>
-            <div className="w-[100vw] text-center mb-10">
+        <div className=" h-screen flex flex-col justify-center items-center text-center bg-[#A7A7A7]" >
+            <h1 className="p-5 text-center text-3xl w-4/5 font-bold" >Dashboard</h1>
+            <div className=" text-center mb-5 mt-5 w-4/5">
                 <input className="w-2/3 border border-sky-950 rounded-md text-center h-[2.25rem]"
                 type="search"
-                placeholder="seach your events"
+                placeholder="search your events"
                 //value={searchEvents}
                 />
             </div>
             {/* this is the card component*/}
-            <div className=" flex flex-col flex-grow w-full justify-center items-center">
-                <div className="flex flex-wrap gap-4 border border-teal-700 rounded-lg shadow-lg" >
-                    <div className="p-4 flex flex-col items-center" >
+            <div className=" flex flex-col flex-grow w-full justify-center items-center gap-4">
+                {events.map((event) => (
+                <div  key={event.eventId} className="flex flex-col border border-teal-700 rounded-lg shadow-lg" >
+                    <div className=" w2/3 p-2 flex flex-col items-center justify-center bg-white rounded-lg" >
                         <Image
                         className="rounded-lg"
                         src="/assets/placeholder.jpg"
-                        alt="event name"
+                        alt={event.eventName}
                         width={200}
                         height={200}
                         />
-                        <div>
-                            <h2>Codex</h2>
-                            <p>this is event description</p>
+                            <h2 className="font-bold text-2xl text-center " >{event.eventName}</h2>
+                            <p className="pb-4 flex-wrap break-words" >{event.description}</p>
+                            <Button className="p-2 w-4/5" >Details</Button>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     )
