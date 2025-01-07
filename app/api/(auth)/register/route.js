f135e45dd2d57caf0595868bd02e0309189ebdf0
n-kamalesh <kamaltvt579@gmail.com>
2025-01-08 00:14:14 +0530

    Add Password Modification Route

--- app/api/(auth)/register/route.js
+++ app/api/(auth)/register/route.js
@@ -32,12 +32,12 @@ export async function POST(req) {
 
     if (!email.trim() || !password.trim() || !domain.trim()) {
       return NextResponse.json({
-        error: "Email, password and domain are required",
+        error: "Email, password, and domain are required",
         status: 400,
       });
     }
 
-    if (!domains.includes(domain.toLowerCase())) {
+    if (!domains.includes(domain.trim().toLowerCase())) {
       return NextResponse.json({
         error: "Invalid domain",
         status: 400,
@@ -58,7 +58,7 @@ export async function POST(req) {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const isSuperUser = superUserDomains.includes(domain.toLowerCase());
+    const isSuperUser = superUserDomains.includes(domain.trim().toLowerCase());
 
     const newUser = await db
       .insert(users)
@@ -73,11 +73,60 @@ export async function POST(req) {
     return NextResponse.json({
       message: "Registration successful",
       status: 201,
+      user: {
+        email: newUser.email,
+        domain: newUser.domain,
+        isSuperUser: newUser.isSuperUser,
+      },
     });
   } catch (error) {
     console.error("Error during registration:", error);
     return NextResponse.json({
-      error: error.detail || "Internal Server Error",
+      error: "Internal Server Error",
+      status: 500,
+    });
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    const { email, newPassword } = await req.json();
+
+    if (!email.trim() || !newPassword.trim()) {
+      return NextResponse.json({
+        error: "Email and new password are required",
+        status: 400,
+      });
+    }
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .then((rows) => rows[0]);
+
+    if (!user) {
+      return NextResponse.json({
+        error: "User not found",
+        status: 404,
+      });
+    }
+
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+    await db
+      .update(users)
+      .set({ password: hashedNewPassword })
+      .where(eq(users.email, email));
+
+    return NextResponse.json({
+      message: "Password updated successfully",
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error during password update:", error);
+    return NextResponse.json({
+      error: "Internal Server Error",
       status: 500,
     });
   }
